44cc2fa19d190cdfc87c3d9425804b76406d9400
Prince Dwivedi <dwivediprince313@gmail.com>
2025-10-17 00:46:24 +0530

    Animations added

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -18,19 +18,19 @@ const Home = () => {
 
   return (
     <div>
-      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+      <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
         <Header />
       </div>
 
-      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+      <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
         <SpecialityMenu />
       </div>
 
-      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+      <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
         <TopWorkers />
       </div>
 
-      <div data-aos="fade-up" data-aos-duration="1000" data-aos-delay="200">
+      <div data-aos="fade-up" data-aos-duration="1500" data-aos-delay="300">
         <Banner />
       </div>
     </div>
